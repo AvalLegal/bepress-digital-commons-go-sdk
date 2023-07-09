6e16d5d1aae17e19be580176bb55686ed7928823
Bryson <bryson.meiling@gmail.com>
2023-07-08 20:53:28 -0400

    first commit

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/AvalLegal/bepress-digital-commons-go-sdk
+
+go 1.18
---
+++ init.go
@@ -0,0 +1,14 @@
+/**
+ * Author: Bryson Meiling
+ * File: init.go
+ */
+
+package lib
+
+import (
+	"fmt"
+)
+ 
+ func init() {
+	 fmt.Println("[go-lib] initializing ...")
+ }
